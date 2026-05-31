535d22aec30c97191cb73a51747eb37667db3745
Artist <satyamshiv55@gmail.com>
2026-06-01 01:13:41 +0530

    Update _utilities.py

--- Elevenyts/helpers/_utilities.py
+++ Elevenyts/helpers/_utilities.py
@@ -1,4 +1,18 @@
-# _utilities.py - General Utility Functions
+# ==========================================================
+# Copyright (c) 2026 ArtistBots
+# All Rights Reserved.
+#
+# Project      : ArtistBots API Telegram Music Bot
+# Powered By   : Artist
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @ArtistApibot
+# Channel      : https://t.me/artistbots
+# GitHub       : https://github.com/elevenyts
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 
 import re
 from pyrogram import enums, errors, types
